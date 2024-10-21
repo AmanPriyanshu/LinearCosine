f875f5d868500f3f30c8524192b3449194cb2141
AmanPriyanshu <amanpriyanshusms2001@gmail.com>
2024-10-21 11:23:06 -0400

    updating out of py

--- src/linear_cosine/linear_cosine.py
+++
@@ -1,4 +0,0 @@
-from ._linear_cosine import cosine_similarity as _cosine_similarity
-
-def cosine_similarity(v1, v2, mantissa_bits=8, use_lmul=True):
-    return _cosine_similarity(v1, v2, mantissa_bits, use_lmul)
